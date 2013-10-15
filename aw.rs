3e994e75ed70b3492afca13202589eb63f0466c7
Jake Kaufman <jakeman@foursquare.com>
2013-10-15 02:14:18 +0000

    Add support for variables

--- aw.rs
+++ aw.rs
@@ -1,33 +1,52 @@
+use std::hashmap::HashMap;
+
+type Env = HashMap<~str, int>;
+
 enum Expr {
   Number(int),
+  Var(~str),
   Plus(~Expr, ~Expr),
   Minus(~Expr, ~Expr),
   Times(~Expr, ~Expr)
 }
 
-fn interp(expr: ~Expr) -> int {
+fn lookup(env: &Env, x: ~str) -> int {
+  // TODO: figure out how to error out ourselves here
+  *env.find(~x).get()
+}
+
+fn interp(expr: ~Expr, lookupEnv: &Env) -> int {
   match *expr {
-    Plus(a,b) => interp(a) + interp(b),
-    Minus(a,b) => interp(a) - interp(b),
-    Times(a,b) => interp(a) * interp(b),
-    Number(x) => x
+    Plus(a,b) => interp(a, lookupEnv) + interp(b, lookupEnv),
+    Minus(a,b) => interp(a, lookupEnv) - interp(b, lookupEnv),
+    Times(a,b) => interp(a, lookupEnv) * interp(b, lookupEnv),
+    Number(x) => x,
+    Var(x) => lookup(lookupEnv, x)
   }
 }
 
 fn main() {
+  // TODO: figure out how to literal this
+  let mut lookupEnv: ~Env = ~HashMap::new();
+  lookupEnv.insert(~"x", 5);
+
   let mut expr: ~Expr;
+
   expr = ~Number(5);
-  println(interp(expr).to_str());
+  println(interp(expr, lookupEnv).to_str());
+
+  expr = ~Var(~"x");
+  println(interp(expr, lookupEnv).to_str());
 
   // 5 + 5
-  expr = ~Plus(~Number(5), ~Number(5));
-  println(interp(expr).to_str());
+  expr = ~Plus(~Var(~"x"), ~Number(5));
+  println(interp(expr, lookupEnv).to_str());
 
   // 5 + 5 - 3
-  expr = ~Minus(~Plus(~Number(5), ~Number(5)), ~Number(3));
-  println(interp(expr).to_str());
+  expr = ~Minus(~Plus(~Var(~"x"), ~Number(5)), ~Number(3));
+  println(interp(expr, lookupEnv).to_str());
 
   // 3 * (5 + 5 - 3)
-  expr = ~Times(~Minus(~Plus(~Number(5), ~Number(5)), ~Number(3)), ~Number(3));
-  println(interp(expr).to_str());
+  expr = ~Times(~Minus(~Plus(~Var(~"x"), ~Number(5)), ~Number(3)), ~Number(3));
+  println(interp(expr, lookupEnv).to_str());
 }
